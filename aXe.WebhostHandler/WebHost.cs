9752d0c70e844eaaac493fb29f587932897a521d
Xutianlin <taiki0532@live.com>
2017-11-22 21:56:33 +0800

    Start

---
+++ aXe.WebhostHandler/WebHost.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace aXe.WebhostHandler
+{
+    public class WebHost
+    {
+        private static WebHost host = new WebHost();
+        public static WebHost GetInstance()
+        {
+            return host;
+        }
+
+        public Task HandleRequest(HttpContext c)
+        {
+            c.Response.WriteAsync("hello world");
+            return 
+        }
+    }
+}
---
+++ aXe.Website/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing;
+using aXe.WebhostHandler;
+using Microsoft.AspNetCore.Http;
+
+namespace aXe.Website
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            var webHostEntrance = new RouteHandler((c) => WebHost.GetInstance().HandleRequest(c));
+            //var webHostEntrance = new RouteHandler((c) => c.Response.WriteAsync("Hello, I am Routing!"));
+            var resolver = app.ApplicationServices.GetRequiredService<IInlineConstraintResolver>();
+            var runRoute = new Route(webHostEntrance, "{item}", resolver);
+
+            app.UseRouter(runRoute);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+            });
+        }
+    }
+}
